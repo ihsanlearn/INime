57c0006b7f82e681f6a414e212aab6c129123892
ihsanlearn <iihsannlearn@gmail.com>
2023-10-28 09:53:20 +0700

    ihsan

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,10 @@
+const Navbar = () => {
+    return (
+        <header>
+            <div className="flex">
+                <p>Inime | Website Anime Indonesia</p>
+                <input placeholder="Cari anime..." type="text"/>
+            </div>
+        </header>
+    )
+}
