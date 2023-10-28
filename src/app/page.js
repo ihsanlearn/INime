30a8ba97bfedbc003f3b8d83dbd7737290254e1c
ihsanlearn <iihsannlearn@gmail.com>
2023-10-29 05:42:08 +0700

    Ihsan committed

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Navbar from '@/components/Navbar'
 const Page = () => {
   return (
     <div>
-      <Navbar />
+      <Navbar title="INime | Website Anime Indonesia" />
     </div>
   )
 }
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,10 +1,12 @@
-const Navbar = () => {
+const Navbar = ({ title }) => {
     return (
         <header>
-            <div className="flex">
-                <p>Inime | Website Anime Indonesia</p>
-                <input placeholder="Cari anime..." type="text"/>
+            <div className="flex flex-col items-start bg-slate-800 text-white p-4 md:justify-between md:flex-row md:items-center">
+                <p className="font-semibold mb-3 md:mb-0">{title}</p>
+                <input placeholder="Cari anime..." type="text" className="rounded p-1 md:px-2 w-full md:w-56"/>
             </div>
         </header>
     )
 }
+
+export default Navbar
